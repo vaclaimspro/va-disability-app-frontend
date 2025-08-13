1b2a0a3f2a994c6c525233d5e015a396ea1755c7
vaclaimspro <thevaclaimspro@gmail.com>
2025-08-13 14:01:56 -0400

    Update MembershipPage.jsx

--- src/pages/MembershipPage.jsx
+++ src/pages/MembershipPage.jsx
@@ -6,7 +6,7 @@ import { UserSubscription, UserProfile } from '../models/index.js';
 import { Card, Button } from '../components';
 import { CheckCircleIcon, ExternalLinkIcon } from '../icons';
 
-const API_NAME = 'apistripeapi';
+const API_NAME = 'StripeApi';
 
 export default function MembershipPage({ userData, setPage, isStripeCustomerReady }) {
     const [isLoading, setIsLoading] = useState(false);
